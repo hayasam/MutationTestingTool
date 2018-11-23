64f079feacabc9a7729e9c5003eb8e2d0376800f
Cornillon Arnaud <arnaud.cornillon@insa-rennes.fr>
2018-11-23 11:34:24 +0100

    Refactor return default + TODO + tests intensifs !

--- src/main/java/mutation/ReturnDefaultOperator.java
+++ src/main/java/mutation/ReturnDefaultOperator.java
@@ -4,6 +4,7 @@ import mutationproject.MutationInfo;
 import spoon.reflect.code.CtStatement;
 import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.reference.CtTypeReference;
+import spoon.support.compiler.SnippetCompilationHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,55 +31,20 @@ public class ReturnDefaultOperator extends MutationOperator<CtMethod>
         return super.isToBeProcessed(candidate) && candidate.getType().isPrimitive() && !candidate.getType().equals(getFactory().Type().voidPrimitiveType());
     }
 
-    /* TODO
-    @Test
-	public void testCompileStatementWithReturn() {
-		// contract: a snippet with return can be compiled.
-		CtElement el = SnippetCompilationHelper.compileStatement(
-				factory.Code().createCodeSnippetStatement("return 3"),
-				factory.Type().INTEGER
-		);
-		assertTrue(CtReturn.class.isAssignableFrom(el.getClass()));
-		assertEquals("return 3", el.toString());
-	}
-     */
-
     @Override
     protected MutationInfo applyMutation(CtMethod ctMethod)
     {
         backup = new ArrayList<>(ctMethod.getBody().getStatements());
         ctMethod.getBody().getStatements().clear();
-        CtTypeReference type = ctMethod.getType();
 
-        switch(type.getSimpleName()) // TODO refactoring
-        {
-            case "int":
-                ctMethod.getBody().addStatement(getFactory().<Integer>createReturn().setReturnedExpression(getFactory().Code().createLiteral(0)));
-                break;
-            case "char":
-                ctMethod.getBody().addStatement(getFactory().<Character>createReturn().setReturnedExpression(getFactory().Code().createLiteral((char)0)));
-                break;
-            case "short":
-                ctMethod.getBody().addStatement(getFactory().<Short>createReturn().setReturnedExpression(getFactory().Code().createLiteral((short)0)));
-                break;
-            case "long":
-                ctMethod.getBody().addStatement(getFactory().<Long>createReturn().setReturnedExpression(getFactory().Code().createLiteral(0L)));
-                break;
-            case "byte":
-                ctMethod.getBody().addStatement(getFactory().<Byte>createReturn().setReturnedExpression(getFactory().Code().createLiteral((byte)0)));
-                break;
-            case "float":
-                ctMethod.getBody().addStatement(getFactory().<Float>createReturn().setReturnedExpression(getFactory().Code().createLiteral(0F)));
-                break;
-            case "double":
-                ctMethod.getBody().addStatement(getFactory().<Double>createReturn().setReturnedExpression(getFactory().Code().createLiteral(0D)));
-                break;
-            case "boolean":
-                ctMethod.getBody().addStatement(getFactory().<Boolean>createReturn().setReturnedExpression(getFactory().Code().createLiteral(false)));
-                break;
+        if(ctMethod.getType().getSimpleName().equals("boolean")) {
+            ctMethod.getBody().addStatement(getFactory().<Boolean>createReturn().setReturnedExpression(getFactory().Code().createLiteral(false)));
+        } else {
+            ctMethod.getBody().addStatement(SnippetCompilationHelper.compileStatement(
+                    getFactory().Code().createCodeSnippetStatement("return 0"),
+                    ctMethod.getType()));
         }
-
-        return new MutationInfo("Empty body and return default value", ctMethod.getSimpleName());
+       return new MutationInfo("Empty body and return default value", ctMethod.getSimpleName());
     }
 
     @Override
